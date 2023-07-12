91be8cf246bf0f302669ac4bbc6fa4f7df8ef367
UidSoftware <uidsoftwaretecnologia@gmail.com>
2023-07-11 23:55:51 -0300

    Modificacao 11 de julho

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+
+var bodyParser = require('body-parser');
+
 const  path  = require('path');
 
 const app = express();
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+    extended: true
+}))
 
 app.engine('html', require('ejs').renderFile); //renderizar do tipo html, usando ejs
 app.set('view engine', 'html'); //setando view para html
@@ -9,16 +17,24 @@ app.use('/public', express.static(path.join(__dirname, 'public'))); //tudo que Ã
 app.set('views', path.join(__dirname, '/views')); //pegar diretorio completo.
 
 
-var tarefas = [ 'Arrumar o quarto',
-                'Compras no supermercado',
-                'Arrumar a casa',
-                'Estudar'];
+var tarefas = [ 
+    'Arrumar o quarto',
+    'Compras no supermercado',
+    'Arrumar a casa','Estudar'
+];
+
+app.post('/',(req, res)=>{
+    
+    tarefas.push(req.body.tarefa);
+    res.render('index', { tarefasList:tarefas}); 
+    //res.render('index',{text:'Este site foi criado por Uid Software. CEO Luiz Eduardo'});
+});
 
 app.get('/',(req, res)=>{
 
     /**
      * segundo parametro'{}', serve para renderizar automaticamente
-     * nome:'Luiz Eduardo G Ferreira',
+     * 
      */
    res.render('index', { tarefasList:tarefas}); 
    
@@ -32,7 +48,9 @@ app.get('/deletar/:id',(req,res)=>{
         }
     })
     res.render('index',{tarefasList:tarefas});
-})
+});
+
+
 
 app.listen(3000, ()=>{
     console.log('SERVER RUN!');
